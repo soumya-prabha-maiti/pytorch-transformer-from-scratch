26bfdd76867690f544f07d4ad6110d3b7f6f9eaf
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2024-08-12 01:35:14 +0530

    Add decorator factory to accept boolean flags whether to log shapes & types. Modify the log format and hence the parsing logic for JSON creation. Add separate JSON for shapes and types.

--- log_utils.py
+++ log_utils.py
@@ -18,24 +18,21 @@ class JsonFileHandler(logging.Handler):
 
             # Split the message at double colons
             # The record after formatting is expected to be in the format:
-            #       timestamp::log_level::module_name::func_name::line_num::message
+            #       timestamp::log_level::message
             # where message is in the format:
-            #       nn_block::variable_name::data  (data can be shape, type etc)
+            #       class_name(nn_block)::function_name::variable_name::data  (data can be shape, type etc)
             (
                 timestamp,
                 log_level,
-                module_name,
+                class_name,
                 func_name,
-                line_num,
-                nn_block,
                 variable_name,
                 data,
             ) = [item.strip() for item in log_message.split("::")]
 
-            self.data.setdefault(module_name, {})
-            self.data[module_name].setdefault(func_name, {})
-            self.data[module_name][func_name].setdefault(nn_block, {})
-            self.data[module_name][func_name][nn_block][variable_name] = data
+            self.data.setdefault(class_name, {})
+            self.data[class_name].setdefault(func_name, {})
+            self.data[class_name][func_name][variable_name] = data
 
             with open(self.json_file_path, "w") as json_file:
                 json.dump(self.data, json_file, indent=4)
@@ -43,46 +40,71 @@ class JsonFileHandler(logging.Handler):
             self.handleError(record)
 
 
-def log_types(func):
-    arg_names = inspect.getfullargspec(func).args
+def log_metadata(log_shapes=True, log_types=True):
+    def log_metadata_without_args(func):
+        arg_names = inspect.getfullargspec(func).args
 
-    def wrapper(*args, **kwargs):
-        class_name = func.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0]
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
 
-        func_name = func.__name__
+            class_name = func.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0]
+            func_name = func.__name__
+            args_dict = {name: arg for name, arg in zip(arg_names, args)}
 
-        args_types = {name: type(arg) for name, arg in zip(arg_names, args)}
-        kwargs_types = {k: type(v) for k, v in kwargs.items()}
-        all_input_types = {**args_types, **kwargs_types}
+            all_input_types = {}
+            all_input_shapes = {}
+            for name, arg in {**args_dict, **kwargs}.items():
+                all_input_types[f"{name}"] = type(arg)
+                try:
+                    all_input_shapes[f"{name}.shape"] = arg.shape
+                except:
+                    try:
+                        all_input_shapes[f"{name}.__len__()"] = len(arg)
+                    except:
+                        all_input_shapes[f"{name}"] = "shape or len not available"
 
-        for key, val in all_input_types.items():
-            type_logger.debug(f"{class_name} :: {func_name} :: input.{key} :: {val}")
+            if log_types:
+                for name, val in all_input_types.items():
+                    type_logger.debug(f"{class_name} :: {func_name} :: input.{name} :: {val}")
+                type_logger.debug(f"{class_name} :: {func_name} :: output :: {type(result)}")
 
-        result = func(*args, **kwargs)
+            if log_shapes:
+                for name, val in all_input_shapes.items():
+                    shape_logger.debug(f"{class_name} :: {func_name} :: input.{name} :: {val}")
+                try:
+                    shape_logger.debug(f"{class_name} :: {func_name} :: output.shape :: {result.shape}")
+                except:
+                    try:
+                        shape_logger.debug(f"{class_name} :: {func_name} :: output.__len__() :: {len(result)}")
+                    except:
+                        shape_logger.debug(f"{class_name} :: {func_name} :: output :: shape or len not available")
 
-        type_logger.debug(f"{class_name} :: {func_name} :: output :: {type(result)}")
-        return result
+            return result
 
-    return wrapper
+        return wrapper
+    
+    return log_metadata_without_args
+
 
 
 log_folder = get_log_folder(get_config())
 
-log_formatter = logging.Formatter(
-    "%(asctime)s :: %(levelname)s :: %(module)s.py :: %(funcName)s :: Line %(lineno)d :: %(message)s"
-)
+log_formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
 
 shape_logger = logging.getLogger(__name__ + ".shape_logger")
 shape_logger.setLevel(logging.DEBUG)
 shape_log_filehandler = logging.FileHandler(f"{log_folder}/shapes.log", mode="w")
 shape_log_filehandler.setFormatter(log_formatter)
 shape_logger.addHandler(shape_log_filehandler)
-json_handler = JsonFileHandler(f"{log_folder}/shapes.json")
-json_handler.setFormatter(log_formatter)
-shape_logger.addHandler(json_handler)
+shape_json_filehandler = JsonFileHandler(f"{log_folder}/shapes.json")
+shape_json_filehandler.setFormatter(log_formatter)
+shape_logger.addHandler(shape_json_filehandler)
 
 type_logger = logging.getLogger(__name__ + ".type_logger")
 type_logger.setLevel(logging.DEBUG)
 type_log_filehandler = logging.FileHandler(f"{log_folder}/types.log", mode="w")
 type_log_filehandler.setFormatter(log_formatter)
 type_logger.addHandler(type_log_filehandler)
+type_json_filehandler = JsonFileHandler(f"{log_folder}/types.json")
+type_json_filehandler.setFormatter(log_formatter)
+type_logger.addHandler(type_json_filehandler)
